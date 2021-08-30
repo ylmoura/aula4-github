35ee40227f8dd61f442bcd25aea67d285f5a43af
ylmoura <yvesluan016@gmail.com>
2021-08-30 16:35:00 -0300

    Projeto iNTERFACES

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,40 @@
+package model.services;
+
+import model.entitites.CarRental;
+import model.entitites.Invoice;
+
+public class RentalService {
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		super();
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		 long t1 = carRental.getStart().getTime(); 
+		 long t2 = carRental.getFinish().getTime();
+		 
+		 double hours = (t2 - t1) / 1000 / 60 / 60;  // resultado da data em hora
+		 
+		 double basicPayment;
+		 if (hours  <= 12.0) {
+			 basicPayment = Math.ceil(hours) *  pricePerHour;
+		 }
+		 else {
+			 basicPayment = Math.ceil(hours/ 24) * pricePerDay;
+		 }
+		 
+		 double tax = taxService.tax(basicPayment);
+		 
+		 carRental.setInvoice(new Invoice(basicPayment,tax));
+		 
+	}
+	
+	
+}
